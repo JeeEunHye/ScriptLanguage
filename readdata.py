49bed20b99b8e3811b4bd39f4b535db8b89f9432
JeeEunHye <jeh0417@naver.com>
2018-06-14 00:19:38 +0900

    tkinter GUI 수정, 이메일 보내기 기능, 지도 연동 기능

--- readdata.py
+++ readdata.py
@@ -3,10 +3,9 @@ from bs4 import BeautifulSoup
 import urllib.request as req
 import urllib.parse
 
-Data = None
 
 def LoadXMLFromFile():
-    global Data
+    Data = None
     url = "http://apis.data.go.kr/B552657/ErmctInfoInqireService/getEgytListInfoInqire?serviceKey=uiHZLqJ3xI1D%2B6DAcB5SF3mJL5rcFqCqo628wClXgoesJSMUUVqGPZJqgMiIzvErTq6%2BwQRFcr27%2BuVERdvuIQ%3D%3D&pageNo=1&startPage=1&numOfRows=408&pageSize=408"
     savename = 'data.xml'
 
@@ -19,50 +18,16 @@ def LoadXMLFromFile():
     Data = BeautifulSoup(xml, "html.parser")
     return Data
 
-def PrintDataList():
-    global Data
-    for location in Data.find_all("item"):
-        print("기관명     : ", location.dutyname.string)
-        print("주소       : ", location.dutyaddr.string)
-        print("분류명     : ", location.dutyemclsname.string)
-        print("분류       : ", location.dutyemcls.string)
-        print("대표전화   : ", location.dutytel1.string)
-        try:
-            print("응급전화   : ", location.dutytel3.string)
-        except:
-            print("")
+def LoadAPIFromIP():
+    IPData = None
+    url = "http://ip-api.com/xml"
+    savename = 'myipinfo.xml'
 
-        print("")
-        print("")
+    data = urllib.request.urlopen(url).read()
+    text = data.decode('utf-8')
 
-def SearchDataName(keyword):
-    global Data
-    for location in Data.find_all("item"):
-        if keyword in location.dutyname.string:
-            print("기관명     : ", location.dutyname.string)
-            print("주소       : ", location.dutyaddr.string)
-            print("분류명     : ", location.dutyemclsname.string)
-            print("분류       : ", location.dutyemcls.string)
-            print("대표전화   : ", location.dutytel1.text)
-            try:
-                print("응급전화   : ", location.dutytel3.string)
-            except:
-                print("")
-            print("")
-            print("")
+    req.urlretrieve(url, savename)
 
-def SearchDataAddress(keyword):
-    global Data
-    for location in Data.find_all("item"):
-        if keyword in location.dutyaddr.string:
-            print("기관명     : ", location.dutyname.string)
-            print("주소       : ", location.dutyaddr.string)
-            print("분류명     : ", location.dutyemclsname.string)
-            print("분류       : ", location.dutyemcls.string)
-            print("대표전화   : ", location.dutytel1.text)
-            try:
-                print("응급전화   : ", location.dutytel3.string)
-            except:
-                print("")
-            print("")
-            print("")
+    xml = open(savename, "r", encoding="utf-8").read()
+    IPData = BeautifulSoup(xml, "html.parser")
+    return IPData
